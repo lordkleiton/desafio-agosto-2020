2fac7dc8feafdd7729b657779d38adb0ee82aba2
lordkleiton <m_s_r@outlook.com>
2020-08-21 02:22:37 -0300

    primeiros resultados com login/logout

---
+++ src/app/slices/userSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { auth } from "firebase";
+import { auth as localAuth } from "../firebase";
+
+const initialState = {
+  user: null,
+  signedIn: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, { payload }) => {
+      state.user = payload;
+    },
+    setSignedIn: (state, { payload }) => {
+      state.signedIn = payload;
+    },
+    reset: () => initialState,
+  },
+});
+
+const { setUser, setSignedIn, reset } = userSlice.actions;
+
+const login = () => (dispatch) => {
+  localAuth()
+    .signInWithPopup(new auth.GoogleAuthProvider())
+    .then((r) => {
+      dispatch(setUser(JSON.parse(JSON.stringify(r.user))));
+      dispatch(setSignedIn(true));
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const logout = () => (dispatch) => {
+  localAuth()
+    .signOut()
+    .then(() => {
+      dispatch(reset());
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const activeUser = (state) => state.user.user;
+
+export { login, logout, activeUser };
+
+export default userSlice.reducer;
