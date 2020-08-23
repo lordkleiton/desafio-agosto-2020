d7375a83dc59d247175748ca9f5e5bba3d98a355
lordkleiton <m_s_r@outlook.com>
2020-08-23 19:14:35 -0300

    layout pagina login

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,35 +1,77 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { login, activeUser, loggedIn } from "../../app/slices/user";
-import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../../app/slices/user";
+import { useHistory } from "react-router-dom";
+import {
+  Paper,
+  Button,
+  Avatar,
+  Typography,
+  makeStyles,
+  Grid,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+const styles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(2, 1),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  icon: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  btn: {
+    margin: theme.spacing(2),
+  },
+}));
 
 const Login = () => {
   const history = useHistory();
-
-  const user = useSelector(activeUser);
-  const signedIn = useSelector(loggedIn);
   const dispatch = useDispatch();
+  const classes = styles();
 
   return (
-    <div>
-      <Link to="/despesas">despesas</Link>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={5}>
+        <Paper>
+          <div className={classes.container}>
+            <Avatar className={classes.icon}>
+              <LockOutlinedIcon />
+            </Avatar>
 
-      {signedIn && <div>{user.displayName}</div>}
+            <Typography component="h1" variant="h6" align="center">
+              Faça login com uma conta do Google para prosseguir.
+            </Typography>
 
-      {!signedIn && (
-        <button
-          onClick={() => {
-            const onSuccess = () => {
-              history.replace("/");
-            };
+            <Button
+              variant="contained"
+              fullWidth
+              className={classes.btn}
+              color="primary"
+              onClick={() => {
+                const onSuccess = () => {
+                  history.replace("/");
+                };
 
-            dispatch(login(onSuccess));
-          }}
-        >
-          login
-        </button>
-      )}
-    </div>
+                dispatch(login(onSuccess));
+              }}
+            >
+              login
+            </Button>
+          </div>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
