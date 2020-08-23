1a6c426d8633346d6b9f3ae38f5b071f69f6d271
lordkleiton <m_s_r@outlook.com>
2020-08-23 20:04:05 -0300

    movendo entre rotas

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,40 @@
 import React from "react";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { loggedIn } from "../app/slices/user";
 
 const NavBar = () => {
+  const history = useHistory();
   const signedIn = useSelector(loggedIn);
 
   return signedIn ? (
     <AppBar position="static">
       <Toolbar>
-        <Button color="inherit">Login</Button>
+        <Button
+          color="inherit"
+          onClick={() => {
+            history.push("/despesas");
+          }}
+        >
+          despesas
+        </Button>
+        <Button
+          color="inherit"
+          onClick={() => {
+            history.push("/receitas");
+          }}
+        >
+          receitas
+        </Button>
+        <Button
+          color="inherit"
+          onClick={() => {
+            history.push("/info");
+          }}
+        >
+          informações
+        </Button>
       </Toolbar>
     </AppBar>
   ) : null;
