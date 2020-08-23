258d1ed782af12c686b272a6ecb5718028a2febb
lordkleiton <m_s_r@outlook.com>
2020-08-23 01:37:15 -0300

    removendo tutoriais do projeto

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-/* reducers */
-
-import counter from "../features/counter/counterSlice";
 import user from "./slices/user";
 import despesas from "./slices/despesas";
 import receitas from "./slices/receitas";
 
 export default configureStore({
   reducer: {
-    counter,
     user,
     despesas,
     receitas,
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { activeUser, loggedIn } from "../../app/slices/user";
 import { Link } from "react-router-dom";
 
