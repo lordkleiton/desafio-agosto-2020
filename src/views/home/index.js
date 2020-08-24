e68cd92873779046949a03feabe2689d2210d102
lordkleiton <m_s_r@outlook.com>
2020-08-24 01:35:50 -0300

    visual final home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { activeUser, loggedIn } from "../../app/slices/user";
-import { Link } from "react-router-dom";
+import { activeUser } from "../../app/slices/user";
+import { Typography, Grid } from "@material-ui/core";
 
 const Home = () => {
   const user = useSelector(activeUser);
-  const signedIn = useSelector(loggedIn);
+  const firstName = user.displayName.split(" ")[0];
 
   return (
     <div>
-      {!signedIn && (
-        <div>
-          <p>Faça login para continuar</p>
-          <Link to="/login">Login</Link>
-        </div>
-      )}
-      {signedIn && (
-        <div>
-          <div>{user.displayName}</div>
-        </div>
-      )}
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={5}>
+          <Typography component="h1" align="center">
+            Olá, {firstName}.
+          </Typography>
+          <Typography component="h1" align="center">
+            Navegue através da barra superior.
+          </Typography>
+        </Grid>
+      </Grid>
     </div>
   );
 };
